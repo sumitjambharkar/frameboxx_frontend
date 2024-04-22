b05ce4c76f972bc666c1d4dce80b967603640161
sumitjambharkar <sujam3545@gmail.com>
2024-04-22 11:23:34 +0530

    commit

--- src/Pages/Contact.js
+++ src/Pages/Contact.js
@@ -1,131 +1,169 @@
-import axios from 'axios'
-import React, { useState } from 'react'
+import axios from "axios";
+import React, { useState } from "react";
+import Swal from "sweetalert2";
 
 const Contact = () => {
   const [data, setData] = useState({
-    name:"",
-    email:"",
-    phone:"",
-    message:"",
-    course:""
-  })
+    name: "",
+    email: "",
+    phone: "",
+    message: "",
+    course: "",
+  });
 
   const handleChange = (e) => {
-    setData({...data,[e.target.name]:e.target.value})
-  }
+    setData({ ...data, [e.target.name]: e.target.value });
+  };
 
-  const send = async () => {
+  const send = async (e) => {
+    e.preventDefault();
     try {
-      const response = await axios.post('https://frameboxx-backend.onrender.com/submit-form', {
-        name: data.name,
-        email: data.email,
-        phone: data.phone,
-        message: data.message,
-        course: data.course
+      const response = await axios.post(
+        "https://frameboxx-backend.onrender.com/submit-form",
+        {
+          name: data.name,
+          email: data.email,
+          phone: data.phone,
+          message: data.message,
+          course: data.course,
+        }
+      );
+      Swal.fire({
+        title: "Good job!",
+        text: "Added!",
+        icon: "success",
       });
-
-      console.log("Response:", response.data);
-      // Handle success if needed
+      setData({
+        name: "",
+        email: "",
+        phone: "",
+        message: "",
+        course: "",
+      });
     } catch (error) {
       console.error("Error:", error);
-      // Handle error if needed
     }
   };
 
   return (
     <div>
-       <div>
+      <div>
         <div>
-            <h4  className="hedtitle">Get in Touch With Us</h4>
+          <h4 className="hedtitle">Get in Touch With Us</h4>
         </div>
-      <div class="formbold-main-wrapper">
-        <div class="formbold-form-wrapper">
-          <div>
-            <div class="formbold-input-flex">
-              <div>
-                <input
-                  value={data.name}
+        <div class="formbold-main-wrapper">
+          <form onSubmit={send} class="formbold-form-wrapper">
+            <div>
+              <div class="formbold-input-flex">
+                <div>
+                  <input
+                    required
+                    value={data.name}
+                    onChange={handleChange}
+                    type="text"
+                    name="name"
+                    id="firstname"
+                    class="formbold-form-input"
+                  />
+                  <label for="firstname" class="formbold-form-label">
+                    {" "}
+                    Full Name{" "}
+                  </label>
+                </div>
+                <div>
+                  <select
+                    required
+                    value={data.course}
+                    onChange={handleChange}
+                    name="course"
+                    id="course"
+                    className="formbold-form-input"
+                  >
+                    <option value="Choose Courses">Choose Courses</option>
+                    <option value="Degree in Animation & VFX">
+                      Degree in Animation & VFX
+                    </option>
+                    <option value="Animation Courses">Animation Courses</option>
+                    <option value="Game Design/Game Art Courses">
+                      Game Design/Game Art Courses
+                    </option>
+                    <option value="Web Design & Development Courses">
+                      Web Design & Development Courses
+                    </option>
+                    <option value="Visual Effects (VFX) Courses">
+                      Visual Effects (VFX) Courses
+                    </option>
+                    <option value="UI & UX Course,Motion Graphic Courses">
+                      UI & UX Course,Motion Graphic Courses
+                    </option>
+                    <option value="Graphic Designing Courses">
+                      Graphic Designing Courses
+                    </option>
+                  </select>
+                  <label for="lastname" class="formbold-form-label">
+                    Choose Courses
+                  </label>
+                </div>
+              </div>
+
+              <div class="formbold-input-flex">
+                <div>
+                  <input
+                    required
+                    value={data.email}
+                    onChange={handleChange}
+                    type="email"
+                    name="email"
+                    id="email"
+                    class="formbold-form-input"
+                  />
+                  <label for="email" class="formbold-form-label">
+                    {" "}
+                    Mail{" "}
+                  </label>
+                </div>
+                <div>
+                  <input
+                    required
+                    value={data.phone}
+                    onChange={handleChange}
+                    type="text"
+                    name="phone"
+                    id="phone"
+                    class="formbold-form-input"
+                  />
+                  <label for="phone" class="formbold-form-label">
+                    {" "}
+                    Phone{" "}
+                  </label>
+                </div>
+              </div>
+
+              <div class="formbold-textarea">
+                <textarea
+                  required
+                  value={data.message}
                   onChange={handleChange}
-                  type="text"
-                  name="name"
-                  id="firstname"
+                  rows="6"
+                  name="message"
+                  id="message"
                   class="formbold-form-input"
-                />
-                <label for="firstname" class="formbold-form-label">
+                ></textarea>
+                <label for="message" class="formbold-form-label">
                   {" "}
-                  Full Name{" "}
+                  Message{" "}
                 </label>
               </div>
-              <div>
-              <select value={data.course} onChange={handleChange} name="course" id="course" className="formbold-form-input">
-        <option value="Choose Courses">Choose Courses</option>
-        <option value="Degree in Animation & VFX">Degree in Animation & VFX</option>
-        <option value="Animation Courses">Animation Courses</option>
-        <option value="Game Design/Game Art Courses">Game Design/Game Art Courses</option>
-        <option value="Web Design & Development Courses">Web Design & Development Courses</option>
-        <option value="Visual Effects (VFX) Courses">Visual Effects (VFX) Courses</option>
-        <option value="UI & UX Course,Motion Graphic Courses">UI & UX Course,Motion Graphic Courses</option>
-        <option value="Graphic Designing Courses">Graphic Designing Courses</option>
-      </select>
-                <label for="lastname" class="formbold-form-label">
-                  Choose Courses
-                </label>
-              </div>
-            </div>
 
-            <div class="formbold-input-flex">
-              <div>
-                <input
-                   value={data.email}
-                   onChange={handleChange}
-                  type="email"
-                  name="email"
-                  id="email"
-                  class="formbold-form-input"
-                />
-                <label for="email" class="formbold-form-label">
-                  {" "}
-                  Mail{" "}
-                </label>
-              </div>
-              <div>
-                <input
-                 value={data.phone}
-                 onChange={handleChange}
-                  type="text"
-                  name="phone"
-                  id="phone"
-                  class="formbold-form-input"
-                />
-                <label for="phone" class="formbold-form-label">
-                  {" "}
-                  Phone{" "}
-                </label>
-              </div>
+              <button type="submit" class="button-81">
+                Submit
+              </button>
             </div>
-
-            <div class="formbold-textarea">
-              <textarea
-               value={data.message}
-               onChange={handleChange}
-                rows="6"
-                name="message"
-                id="message"
-                class="formbold-form-input"
-              ></textarea>
-              <label for="message" class="formbold-form-label">
-                {" "}
-                Message{" "}
-              </label>
-            </div>
-
-            <button onClick={send} class="button-81">Submit</button>
-          </div>
+          </form>
         </div>
       </div>
     </div>
-    </div>
-  )
-}
+  );
+};
 
-export default Contact
+export default Contact;
